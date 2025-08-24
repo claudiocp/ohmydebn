c8abb0e39322b1ade3d32443f16267eedad69e13
Doug Burks <doug.burks@securityonionsolutions.com>
2025-08-24 07:28:02 -0400

    clean up output

--- ohmydebn.sh
+++ ohmydebn.sh
@@ -258,7 +258,7 @@ fi
 
 ALACRITTY_DIR=~/.config/alacritty
 ALACRITTY_CONFIG=$ALACRITTY_DIR/alacritty.toml
-if grep -q "alacritty/catppuccin-mocha.toml" $ALACRITTY_CONFIG >/dev/null; then
+if grep -q "alacritty/catppuccin-mocha.toml" $ALACRITTY_CONFIG >/dev/null 2>&1; then
   mv $ALACRITTY_DIR $ALACRITTY_DIR.before.themes
 fi
 
