8d0f28d7e24b2fb85362f050c5247dfe94a2f33f
Doug Burks <doug.burks@securityonionsolutions.com>
2025-08-27 17:05:30 -0400

    improve theme support

--- ohmydebn.sh
+++ ohmydebn.sh
@@ -259,7 +259,7 @@ done
 display "tte rain" "Installing new apps if unnecessary and configuring them"
 sudo DEBIAN_FRONTEND=noninteractive apt -y install alacritty bat binutils btop cava chromium curl eza fzf \
   git gimp golang gum gvfs-backends htop iperf3 keepassxc libnotify-bin neovim openvpn pdftk-java python-is-python3 \
-  ripgrep ristretto rofi screenfetch starship systemd-timesyncd vim wget xdotool yaru-theme-icon yq \
+  ripgrep ristretto rofi screenfetch starship systemd-timesyncd vim wget xdotool yaru-theme-gtk yaru-theme-icon yq \
   zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
 
 display "cat" "Setting alacritty as default terminal emulator"
