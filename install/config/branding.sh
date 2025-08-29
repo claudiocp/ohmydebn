abad00ab21de5c17af2ecfa55860fb5faae91610
Doug Burks <doug.burks@securityonionsolutions.com>
2025-08-29 15:40:59 -0400

    initial support for screensaver

---
+++ install/config/branding.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Allow the user to change the branding for screenfetch and screensaver
+mkdir -p ~/.config/ohmydebn/branding
+#cp ~/.local/share/ohmydebn/icon.txt ~/.config/ohmydebn/branding/about.txt
+toilet -f mono12 OhMyDebn >~/.config/ohmydebn/branding/screensaver.txt
