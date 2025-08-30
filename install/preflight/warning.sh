5ec4e0352fe693ad8efc5d62eb5c9d8ec3d77320
Doug Burks <doug.burks@securityonionsolutions.com>
2025-08-30 15:45:55 -0400

    refactor warning into warning.sh

---
+++ install/preflight/warning.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -f $STATE_FILE ]; then
+  display "cat" "WARNING!
+
+This script:
+- is intended for a clean new installation.
+$(if [ "$NO_UNINSTALL" = false ]; then echo "- will remove apps like FireFox, Thunderbird, and others."; else echo "- will NOT remove any existing packages (--no-uninstall mode)."; fi)
+- may make changes to your APT configuration.
+
+This script is totally unsupported. 
+If it breaks your system, you get to keep both pieces!
+
+Press Enter to continue or Ctrl-c to cancel."
+  read input
+fi
--- ohmydebn.sh
+++ ohmydebn.sh
@@ -15,21 +15,7 @@ source $OHMYDEBN_INSTALL/preflight/functions.sh
 source $OHMYDEBN_INSTALL/preflight/os.sh
 source $OHMYDEBN_INSTALL/preflight/user.sh
 source $OHMYDEBN_INSTALL/preflight/arguments.sh
-
-if [ ! -f $STATE_FILE ]; then
-  display "cat" "WARNING!
-
-This script:
-- is intended for a clean new installation.
-$(if [ "$NO_UNINSTALL" = false ]; then echo "- will remove apps like FireFox, Thunderbird, and others."; else echo "- will NOT remove any existing packages (--no-uninstall mode)."; fi)
-- may make changes to your APT configuration.
-
-This script is totally unsupported. 
-If it breaks your system, you get to keep both pieces!
-
-Press Enter to continue or Ctrl-c to cancel."
-  read input
-fi
+source $OHMYDEBN_INSTALL/preflight/warning.sh
 
 if ! dpkg -s curl >/dev/null 2>&1 ||
   ! dpkg -s libglib2.0-bin >/dev/null 2>&1 ||
