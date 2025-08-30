0f3086eead32a508e8c774aba31f2c2f7a3398bc
Doug Burks <doug.burks@securityonionsolutions.com>
2025-08-30 07:09:24 -0400

    fix path.sh

--- install/path.sh
+++ install/path.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for FILE in ~/.bashrc ~/.zshrc
-if ! grep ".local/share/ohmydebn/bin" $FILE > /dev/null 2>&1; then
-  echo "export PATH=$HOME/.local/share/ohmydebn/bin:$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH" >> $FILE
-fi
+for FILE in ~/.bashrc ~/.zshrc; do
+  if ! grep ".local/share/ohmydebn/bin" $FILE >/dev/null 2>&1; then
+    echo "export PATH=$HOME/.local/share/ohmydebn/bin:$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH" >>$FILE
+  fi
+done
