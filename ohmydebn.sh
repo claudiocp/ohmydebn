03673c6799b948dbbab3bede50d3cd2991aeb1c9
Doug Burks <doug.burks@securityonionsolutions.com>
2025-08-30 06:40:39 -0400

    remove old files from /usr/local/bin/

--- ohmydebn.sh
+++ ohmydebn.sh
@@ -168,9 +168,12 @@ if [ -f /usr/bin/pveversion ] && ! dpkg -s dbus-x11 >/dev/null 2>&1; then
   export $(dbus-launch)
 fi
 
-display "cat" "Copying binaries to /usr/local/bin/"
-sudo cp -av ~/.local/share/$PROJECT_LOWER/bin/* /usr/local/bin/
-sudo chmod +x /usr/local/bin/$PROJECT_LOWER*
+for FILE in ohmydebn-demo ohmydebn-demo-gui ohmydebn-keepass ohmydebn-logo ohmydebn-logo-generate ohmydebn-logo-gui ohmydebn-reset-config ohmydebn-rofi-window-switcher ohmydebn-screenfetch ohmydebn-screenfetch-gui ohmydebn-show-done ohmydebn-show-logo ohmydebn-theme-bg-next ohmydebn-theme-current ohmydebn-theme-install ohmydebn-theme-list ohmydebn-theme-next ohmydebn-theme-remove ohmydebn-theme-set ohmydebn-theme-set-gui ohmydebn-update ohmydebn-update-available ohmydebn-update-components ohmydebn-update-git ohmydebn-update-system-pkgs ohmydebn-version; do
+  if [ -f /usr/local/bin/$FILE ]; then
+    echo "Removing old file /usr/local/bin/$FILE"
+    rm -f /usr/local/bin/$FILE
+  fi
+done
 
 display "cat" "Updating themes"
 if [ ! -d ~/.local/share/omarchy ]; then
