ca34c829ff9c3b73679baf7e6933c792885a03ca
Doug Burks <doug.burks@securityonionsolutions.com>
2025-08-31 06:44:49 -0400

    remove starship script

--- install/config/starship.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-STARSHIP_CONFIG=~/config/starship.toml
-if [ ! -f $STARSHIP_CONFIG ]; then
-  display "cat" "Configuring starship prompt"
-  cp ~/.local/share/$PROJECT_LOWER/config/starship.toml ~/.config/
-fi
