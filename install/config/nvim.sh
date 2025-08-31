1f9bf8078bc4c5bf7a6838fe3089f2b224c7d16a
Doug Burks <doug.burks@securityonionsolutions.com>
2025-08-31 06:20:58 -0400

    disable nvim relative numbers

--- install/config/nvim.sh
+++ install/config/nvim.sh
@@ -6,12 +6,14 @@ if [ ! -d $NVIM_CONFIG_DIR ]; then
   git clone https://github.com/LazyVim/starter $NVIM_CONFIG_DIR
   rm -rf $NVIM_CONFIG_DIR/.git
 fi
+
 NVIM_PLUGINS=$NVIM_CONFIG_DIR/lua/plugins
 mkdir -p $NVIM_PLUGINS
 if grep -q "colorscheme = \"catppuccin\"" $NVIM_PLUGINS/core.lua >/dev/null 2>&1; then
   display "cat" "Disabling old static neovim theme config"
   mv $NVIM_PLUGINS/core.lua $NVIM_PLUGINS/core.lua.disabled
 fi
+
 NVIM_THEME=$NVIM_PLUGINS/theme.lua
 if [ ! -L $NVIM_THEME ]; then
   display "cat" "Configuring neovim theme"
@@ -19,3 +21,8 @@ if [ ! -L $NVIM_THEME ]; then
 fi
 display "cat" "Updating neovim config"
 cp -av ~/.local/share/$PROJECT_LOWER/config/nvim ~/.config/
+
+NVIM_OPTIONS=$NVIM_CONFIG_DIR/lua/config/options.lua
+if ! grep -q "vim.opt.relativenumber = false" $NVIM_OPTIONS >/dev/null 2>&1; then
+  echo "vim.opt.relativenumber = false" >>$NVIM_OPTIONS
+fi
