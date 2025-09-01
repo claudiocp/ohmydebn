f8605d877c8daef1295b0eb3c4b626ee8c606445
Doug Burks <doug.burks@securityonionsolutions.com>
2025-09-01 05:45:50 -0400

    remove screenfetch from finale

--- install/finalization/finale.sh
+++ install/finalization/finale.sh
@@ -6,8 +6,6 @@ if [ -f ~/.local/state/ohmydebn ]; then
 else
   display "tte rain" "Installation complete!"
   echo
-  screenfetch -N | tte slide --merge
-  echo
   welcome
   # Create a state file signifying that installation is complete
   mkdir -p ~/.local/state
