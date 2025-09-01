9bc89c5075184fb1b0b250998381fab4bfe53045
Doug Burks <doug.burks@securityonionsolutions.com>
2025-09-01 07:44:53 -0400

    add back more headlines

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo; echo "<< Updating themes >>"
+~/.local/share/ohmydebn/bin/ohmydebn-headline "tte rain" "Updating themes"
 
 if [ ! -d ~/.local/share/omarchy ]; then
   cd ~/.local/share/
@@ -26,7 +26,7 @@ for f in ~/.local/share/omarchy/themes/*; do
 done
 
 if [ ! -f ~/.local/state/ohmydebn ]; then
-  echo; echo "<< Setting default theme >>"
+  ~/.local/share/ohmydebn/bin/ohmydebn-headline "tte rain" "Setting default theme"
   mkdir -p ~/.config/ohmydebn/current
   ~/.local/share/ohmydebn/bin/ohmydebn-theme-set Ohmydebn
 fi
