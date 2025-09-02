aa4711db9506928ac45fbe623972e1e7cfbf182c
Doug Burks <doug.burks@securityonionsolutions.com>
2025-09-02 10:23:39 -0400

    update rofi config

--- install/config/rofi.sh
+++ install/config/rofi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-if [ ! -d ~/.config/rofi ]; then
+ROFI_CONFIG=~/.config/rofi
+
+# If we have the old config, then rename it so we can add the new config
+if [ -f $ROFI_CONFIG/catppuccin-default.rasi ]; then
+  mv $ROFI_CONFIG $ROFI_CONFIG-$(date +%Y%m%d-%H%M%S)
+fi
+
+if [ ! -d $ROFI_CONFIG ]; then
   ~/.local/share/ohmydebn/bin/ohmydebn-headline "cat" "Configuring rofi"
   cp -av ~/.local/share/ohmydebn/config/rofi ~/.config/
   echo
