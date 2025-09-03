09b9a46be15f7fb582ddf4de9ab37563f7d4def5
Doug Burks <doug.burks@securityonionsolutions.com>
2025-09-03 11:12:43 -0400

    improve nvim config updates

--- install/config/nvim.sh
+++ install/config/nvim.sh
@@ -21,8 +21,23 @@ if [ ! -L $NVIM_THEME ]; then
   ln -snf ~/.config/ohmydebn/current/theme/neovim.lua $NVIM_THEME
 fi
 
-~/.local/share/ohmydebn/bin/ohmydebn-headline "cat" "Updating neovim config"
-cp -av ~/.local/share/ohmydebn/config/nvim ~/.config/
+LAZYVIM=$NVIM_CONFIG_DIR/lazyvim.json
+if [ ! -f $LAZYVIM ]; then
+  ~/.local/share/ohmydebn/bin/ohmydebn-headline "cat" "Creating $LAZYVIM"
+  cp -av ~/.local/share/ohmydebn/config/nvim/lazyvim.json $LAZYVIM
+fi
+
+SCROLLING=$NVIM_CONFIG_DIR/lua/plugins/snacks-animated-scrolling-off.lua
+if [ ! -f $SCROLLING ]; then
+  ~/.local/share/ohmydebn/bin/ohmydebn-headline "cat" "Creating $SCROLLING"
+  cp -av ~/.local/share/ohmydebn/config/nvim/lua/plugins/snacks-animated-scrolling-off.lua $SCROLLING
+fi
+
+TRANSPARENCY=$NVIM_CONFIG_DIR/plugin/after/transparency.lua
+if [ ! -f $TRANSPARENCY ]; then
+  ~/.local/share/ohmydebn/bin/ohmydebn-headline "cat" "Creating $TRANSPARENCY"
+  cp -av ~/.local/share/ohmydebn/config/nvim/plugin/after/transparency.lua $TRANSPARENCY
+fi
 
 NVIM_OPTIONS=$NVIM_CONFIG_DIR/lua/config/options.lua
 if ! grep -q "vim.opt.relativenumber = false" $NVIM_OPTIONS >/dev/null 2>&1; then
