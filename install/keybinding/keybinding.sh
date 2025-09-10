4e5fe159d8af4d24b01dc22a9807900eff6c9952
Doug Burks <doug.burks@securityonionsolutions.com>
2025-09-10 14:24:41 -0400

    restart cinnamon using setsid so that it will continue running even if session ends

--- install/keybinding/keybinding.sh
+++ install/keybinding/keybinding.sh
@@ -47,7 +47,7 @@ if [ ! -f $KEYBINDING_STATE ]; then
   if pgrep -x cinnamon >/dev/null; then
     ~/.local/share/ohmydebn/bin/ohmydebn-headline "tte rain" "Restarting desktop to apply hotkey configuration"
     sleep 1s
-    /usr/bin/cinnamon --replace >/dev/null 2>&1 &
+    setsid /usr/bin/cinnamon --replace >/dev/null 2>&1
     echo "You can see all hotkeys by pressing Super + K"
   fi
 
